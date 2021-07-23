463810d106d0b391ec727f6ab92d56a39936f758
KONG SANGMIN <86347779+KongSsang@users.noreply.github.com>
2021-07-24 03:38:08 +0900

    Add files via upload

---
+++ codingtest/level1/시저암호.py
@@ -0,0 +1,27 @@
+def solution(s, n):
+    answer = ''
+    from string import ascii_uppercase
+    ua = list(ascii_uppercase)
+    from string import ascii_lowercase
+    la = list(ascii_lowercase)
+
+    for i in range(len(s)):
+        j = 0
+        a = 0
+        if s[i] in ua:
+            j = ua.index(s[i])
+            a = j + n
+            if a > 25:
+                a -= 26
+            answer += ua[a]
+        elif s[i] in la:
+            j = la.index(s[i])
+            a = j + n
+            if a > 25:
+                a -= 26
+            answer += la[a]
+        else:
+            answer += ' '
+    return answer
+
+solution("AB",1)
