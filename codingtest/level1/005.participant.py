1457262b9499784023d16829c82772f63f8dab0e
KONG SANGMIN <86347779+KongSsang@users.noreply.github.com>
2021-07-26 14:48:08 +0900

    coding ex

--- codingtest/level1/005.participant.py
+++ codingtest/level1/005.participant.py
@@ -1,13 +1,17 @@
 def solution(participant, completion):
     answer = ''
     temp = 0
+    part = {}
     
+    for i in participant:
+        part[hash(i)] = i
+
     for i in range(len(participant)):
         temp += hash(participant[i])
 
     for j in range(len(completion)):
-        temp -= hash(participant[j])
-        
-    print(temp)
+        temp -= hash(completion[j])
 
-solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
+    answer = part[temp]
+    
+    return answer
