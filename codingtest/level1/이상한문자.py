cb7020fc37a794dd9d6a8aacf312ae3a2cc4df61
KONG SANGMIN <86347779+KongSsang@users.noreply.github.com>
2021-07-26 14:44:08 +0900

    Add files via upload

---
+++ codingtest/level1/이상한문자.py
@@ -0,0 +1,22 @@
+def solution(s):
+    alp_low = 'abcdefghijklmnopqrstuvwxyz'
+    alp_upp = alp_low.upper()
+    answer = ''
+    l_s = []
+    l_s = s.split(" ")
+
+    for i in range(len(l_s)):
+        y = 0
+        for j in range(0,len(l_s[i])):
+            if (y%2 == 1):
+                temp = l_s[i][j]
+                answer += temp.lower()
+            else:
+                temp = l_s[i][j]
+                answer += temp.upper()
+            y += 1
+        if i == len(l_s)-1:
+            break
+        answer += ' '
+    return answer
+
