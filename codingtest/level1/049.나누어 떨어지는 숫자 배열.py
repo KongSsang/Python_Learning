0ef4e447df145869dd26e5f1769a8f62c331ef03
KONG SANGMIN <86347779+KongSsang@users.noreply.github.com>
2021-10-14 12:20:23 +0800

    Delete 049.나누어 떨어지는 숫자 배열.py

--- codingtest/level1/049.나누어 떨어지는 숫자 배열.py
+++
@@ -1,9 +0,0 @@
-def solution(arr, divisor):
-    answer = []
-    arr = sorted(arr)
-    for i in range(len(arr)):
-        if (arr[i]%divisor) == 0:
-            answer.append(arr[i])
-    if len(answer) == 0:
-        answer.append(-1)
-    return answer
